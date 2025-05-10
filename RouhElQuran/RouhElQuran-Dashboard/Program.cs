8417067361b50c7ce6ceb724b518454f77ff466c
Abdelrahman-Muhmed <abdogomaa18@yahoo.com>
2025-05-10 15:43:26 +0300

    update-ForCompinePublish

--- RouhElQuran/RouhElQuran-Dashboard/Program.cs
+++ RouhElQuran/RouhElQuran-Dashboard/Program.cs
@@ -53,13 +53,13 @@ app.Use(async (context, next) =>
 	context.Request.EnableBuffering();
 	await next();
 });
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//// Configure the HTTP request pipeline.
+////if (!app.Environment.IsDevelopment())
+////{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+////}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
